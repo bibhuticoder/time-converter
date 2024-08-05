4fb1e83fc0a287bec797373d845b7f01d69faa5d
Bibhuti <bibhutipd@gmail.com>
2024-08-05 22:54:16 +0545

    Removes router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './main.scss'
 import TimezonePage from './pages/Timezone.page';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <TimezonePage />,
-  }
-]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
-
+  <TimezonePage />
 )
