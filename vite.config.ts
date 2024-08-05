dfc3e0b5d5a046b6f5db7a700afa8cc890dc0d75
Bibhuti <bibhutipd@gmail.com>
2024-08-05 22:45:49 +0545

    Adds new build with correct base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://bibhuticoder.github.io/",
+  base: "/time-converter/",
   build: {
     outDir: 'docs',
   },
