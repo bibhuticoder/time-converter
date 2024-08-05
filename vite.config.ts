660c842afb6db30dec7f9737057a838ad834b9d1
Bibhuti <bibhutipd@gmail.com>
2024-08-05 22:44:12 +0545

    Adds new build with correct base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://bibhuticoder.github.io/",
   build: {
     outDir: 'docs',
   },
